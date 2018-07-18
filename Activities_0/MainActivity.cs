293b6c6581772d14b58c0e86b542f388b139832d
Jaime Radulovich <jradlov@gmail.com>
2018-07-17 20:14:41 -0700

    Add project files.

---
+++ Activities_0/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Content;
+
+namespace Activities_0
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+			Button btn = FindViewById<Button>(Resource.Id.button1);
+			btn.Click += delegate {
+				var myIntent = new Intent(this, typeof(ShareActivity));
+
+				// if want to pass some data to the Activity:
+				myIntent.PutExtra("myString", "I'm a message passed from Main Activity");
+
+				StartActivity(myIntent);
+			};
+        }
+	}
+}
+
---
+++ Activities_0/ShareActivity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Activities_0
+{
+	[Activity(Label = "ShareActivity")]
+	public class ShareActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+			SetContentView(Resource.Layout.ShareActivityLayout);
+
+			var str = Intent.GetStringExtra("myString");
+			Log.Debug("===== DEBUG =====", "Data: " + str);
+		}
+
+		//===================================================
+		// Optional methods to override default android behavior:
+		// can save the App state in one of these
+
+		protected override void OnPause()
+		{
+			base.OnPause();
+			Log.Debug("===== DEBUG =====", "PAUSE");
+
+			//SaveState();  // save App state 
+		}
+
+		/*void SaveState()    // save App state 
+		{
+			if(!saved) {
+				// save state
+			}
+		}*/
+
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+			Log.Debug("===== DEBUG =====", "DESTROY");
+
+			//SaveState();  // save App state 
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+
+			// LoadState();
+
+			Log.Debug("===== DEBUG =====", "RESUME");
+		}
+
+		public override void OnBackPressed()  
+		{
+			// commenting this out disable the Android back button:
+			// trick to force user to do something (ex: fill out a form)
+			// re-enable back btn after user filled out form
+			base.OnBackPressed();
+			Log.Debug("===== DEBUG =====", "BACK");
+		}
+	}
+}
